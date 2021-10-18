df6fd25e54695a7eb1c3ef76e405e484a421ba77
流星胖胖 <AndyLamwxl@Outlook.com>
2021-10-18 09:45:48 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,22 +1,28 @@
 package com.bytelegend;
+
 /**
+ * 挑战
  * 本类中的注释和Javadoc存在一些问题，会使得`mvn verify`失败。请修复之，确保`mvn verify`通过。
  * 注意：请只修复错误，不要修改注释内容。
- *
+ * <p>
  * There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.
  * Please fix it and make sure `mvn verify` pass.
- * Note that you should only fix the errors, don't modify the comments themselves. */
+ * Note that you should only fix the errors, don't modify the comments themselves.
+ * <p>
+ * Location: C:\\users\\Documents
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) - 3
  *
- * Location: C:\users\Documents
- * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
+ * @author MeteorAndy
  */
 public class Challenge {
     /**
+     * 马克斯
      * Find the maximum integer in three numbers.
      *
      * @param a the first number
      * @param b the second number
      * @param c the third number
+     * @return int
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a,b),c);
