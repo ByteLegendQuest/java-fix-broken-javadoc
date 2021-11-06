489e3e38d8c358e667e7b46079c597e046aedb00
ForsakenUD <imundead@qq.com>
2021-11-06 18:38:58 +0800

    Fix Broken Javadoc

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,13 +2,13 @@ package com.bytelegend;
 /**
  * 本类中的注释和Javadoc存在一些问题，会使得`mvn verify`失败。请修复之，确保`mvn verify`通过。
  * 注意：请只修复错误，不要修改注释内容。
- *
+ * <p>
  * There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves.
- *
- * Location: C:\users\Documents
- * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
+ * <p>
+ * Location: C:{@literal \}users{@literal \}Documents
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) -&gt; 3
  */
 public class Challenge {
     /**
