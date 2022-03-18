389fd7a06a322038f824ed0a20664828865043f7
panxiao81 <panxiao.pp@icloud.com>
2022-03-18 16:20:47 +0000

    Update at 2022-03-18T16:20:47.174576Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,4 +1,5 @@
 package com.bytelegend;
+
 /**
  * 本类中的注释和Javadoc存在一些问题，会使得`mvn verify`失败。请修复之，确保`mvn verify`通过。
  * 注意：请只修复错误，不要修改注释内容。
@@ -6,8 +7,8 @@ package com.bytelegend;
  * There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves. */
- *
- * Location: C:\users\Documents
+/**
+ * Location: C:\\users\Documents
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
  */
 public class Challenge {
