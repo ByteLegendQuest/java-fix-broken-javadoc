f833a1ca4ccbc5626a9210aabce09bf010878d6e
cdrani <charlesdrani@gmail.com>
2022-03-20 06:28:36 +0000

    Update at 2022-03-20T06:28:36.305248Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,6 +17,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
+     * @return max number from a, b, & c
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
