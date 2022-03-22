571aca95e1d346508b144de2c512bbf21b30f987
yunghf <yung.hf@outlook.com>
2022-03-22 10:50:12 +0000

    Update at 2022-03-22T10:50:12.382413Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,14 +1,14 @@
 package com.bytelegend;
 /**
- * <p>本类中的注释和Javadoc存在一些问题，会使得`mvn verify`失败。请修复之，确保`mvn verify`通过。
- * <p>注意：请只修复错误，不要修改注释内容。
+ * <p>本类中的注释和Javadoc存在一些问题，会使得`mvn verify`失败。请修复之，确保`mvn verify`通过。</p>
+ * <p>注意：请只修复错误，不要修改注释内容。</p>
  *
- * <p>There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.
- * <p>Please fix it and make sure `mvn verify` pass.
- * <p>Note that you should only fix the errors, don't modify the comments themselves. 
+ * <p>There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.</p>
+ * <p>Please fix it and make sure `mvn verify` pass.</p>
+ * <p>Note that you should only fix the errors, don't modify the comments themselves. </p>
  *
- * <p>Location: C:\users\Documents
- * <p>Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
+ * <p>Location: C:\users\Documents</p>
+ * <p>Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3</p>
  */
 public class Challenge {
     /**
