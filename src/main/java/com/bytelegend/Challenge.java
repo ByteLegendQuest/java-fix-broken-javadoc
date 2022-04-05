8b08dccae06eda67e5ed68e94f8573c960d2beb3
SamHou2007 <SamHou20070203@163.com>
2022-04-05 10:36:31 +0000

    Update at 2022-04-05T10:36:31.253914Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,4 @@
 package com.bytelegend;
-
 /**
  * 本类中的注释和Javadoc存在一些问题，会使得`mvn verify`失败。请修复之，确保`mvn verify`通过。
  * 注意：请只修复错误，不要修改注释内容。
@@ -8,8 +7,8 @@ package com.bytelegend;
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves.
  *
- * Location: C:&#92;users&#92;Documents
- * Fix the maximum number in three numbers, e.g.{@literal max(1,2,3) -> 3}
+ * Location: C:{@literal \}users{@literal \}Documents
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) -{@literal >} 3
  */
 public class Challenge {
     /**
@@ -18,7 +17,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
-     * @return the maximum integer in three numbers
+     * @return the maximum integer
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
