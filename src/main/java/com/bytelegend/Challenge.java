6b29e98e19210a4c24a36ff06813cc8d13cf7919
hesitatemore <1299136605@qq.com>
2022-04-08 14:47:44 +0000

    Update at 2022-04-08T14:47:44.442534Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -5,9 +5,8 @@ package com.bytelegend;
  *
  * There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.
  * Please fix it and make sure `mvn verify` pass.
- * Note that you should only fix the errors, don't modify the comments themselves. */
- *
- * Location: C:\users\Documents
+ * Note that you should only fix the errors, don't modify the comments themselves.
+ * Location: C:\\users\\Documents
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
  */
 public class Challenge {
@@ -17,6 +16,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
+     * @return the max number of the all param
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
