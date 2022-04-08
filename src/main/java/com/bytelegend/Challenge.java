94249c8169ec6a6cf7b3dc2f01e198cf93d864b9
hesitatemore <1299136605@qq.com>
2022-04-08 14:57:34 +0000

    Update at 2022-04-08T14:57:34.325564Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,8 +6,9 @@ package com.bytelegend;
  * There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves.
+ *
  * Location: C:{@literal \}users{@literal \}Documents
- * Fix the maximum number in three numbers, e.g. max(1,2,3) -{@literal \} 3
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) -{@literal >} 3
  */
 public class Challenge {
     /**
