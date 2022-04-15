1fe07966651fd0556e1810eb7ecf53f130d5feab
g497813927 <admin@techzjc.com>
2022-04-15 05:12:34 +0000

    Update at 2022-04-15T05:12:34.657850Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
-     * @return: the max number
+     * @return the max number
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
