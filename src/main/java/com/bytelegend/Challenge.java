c15bba4103c982f15ac7d9063c7a799955ce56e9
4consultant <996158465@qq.com>
2022-05-19 11:42:23 +0000

    Update at 2022-05-19T11:42:23.491970Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,12 +12,12 @@ package com.bytelegend;
  */
 public class Challenge {
     /**
-     * Find the maximum integer in three numbers.
-     *
-     * @param a the first number
-     * @param b the second number
-     * @param c the third number
-     */
+    * Find the maximum integer in three numbers.
+    *
+    * @param a the first number
+    * @param b the second number
+    * @param c the third number
+    */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
     }
