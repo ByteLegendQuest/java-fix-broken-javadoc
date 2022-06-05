27c4e049ea2c92cacb3cc821bfb93615d52f97a3
1984927068 <1984927068@qq.com>
2022-06-05 09:36:08 +0000

    Update at 2022-06-05T09:36:08.610846Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,11 +12,13 @@ package com.bytelegend;
  */
 public class Challenge {
     /**
-     * Find the maximum integer in three numbers.
-     *
-     * @param  a: the first number
-     * @param  b: the second number
-     * @param  c: the third number
+     * @param a : the first number
+     * @param b : the second number
+     * @param c : the third number
+     * @return: int
+     * @description: Find the maximum integer in three numbers.
+     * @author: Mr.ChiZi
+     * @date: 2022/6/5 17:34
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
