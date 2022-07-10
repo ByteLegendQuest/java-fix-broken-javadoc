6c7fda9dc9c9ec71d3224c131edbbcf5f3032077
sleep-god <wsz135368872127@foxmail.com>
2022-07-10 04:58:28 +0000

    Update at 2022-07-10T04:58:28.439673Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,11 +12,11 @@ package com.bytelegend;
  */
 public class Challenge {
     /**
-     * Find the maximum integer in three numbers.
+     * @see Find the maximum integer in three numbers.
      *
-     * @param :a the first number
-     * @param :b the second number
-     * @param :c the third number
+     * @param a the first number
+     * @param b the second number
+     * @param c the third number
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
