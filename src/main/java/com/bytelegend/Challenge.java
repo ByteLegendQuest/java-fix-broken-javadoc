57d63a1b6e7d7d4d76b8b44a2c4d4d1f5e26d840
sleep-god <wsz135368872127@foxmail.com>
2022-07-13 07:40:05 +0000

    Update at 2022-07-13T07:40:05.215663Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,6 +6,7 @@ package com.bytelegend;
  * There are some issues in the comments and Javadocs in this class, which fails `mvn verify`.
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves. 
+ *
  * Location: C:U+005CusersU+005CDocuments
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -U+003E 3
  */
