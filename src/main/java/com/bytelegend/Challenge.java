93ad8cb9931094979fa3f8f176a8fb81739d08d0
sleep-god <wsz135368872127@foxmail.com>
2022-07-13 07:37:03 +0000

    Update at 2022-07-13T07:37:03.176717Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,7 +7,7 @@ package com.bytelegend;
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves. 
  * Location: C:U+005CusersU+005CDocuments
- * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) -U+003E 3
  */
 public class Challenge {
     /**
