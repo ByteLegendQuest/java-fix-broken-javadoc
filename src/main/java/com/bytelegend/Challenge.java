6235b6aaa102a7dbaab00b0cdd2e80d136330b60
TyCathcart <ty.cathcart.2009+bytelegend@gmail.com>
2022-08-08 22:10:20 +0000

    Update at 2022-08-08T22:10:20.400553Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -8,8 +8,8 @@ package com.bytelegend;
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves.
  *
- * Location: C:\\users\\Documents
- * Fix the maximum number in three numbers, e.g. max(1,2,3) = 3
+ * Location: C:&bsol;users\Documents
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) -&gt; 3
  */
 public class Challenge {
     /**
@@ -18,7 +18,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
-     * @return  Fix the maximum number in three numbers, e.g. max(1,2,3) = 3
+     * @return  Fix the maximum number in three numbers, e.g. max(1,2,3) -&gt; 3
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
