dcdc46b612e623a8f57984257317ce24ed6ebe91
Jonas3012 <jbgood1289@gmail.com>
2022-08-09 16:39:16 +0000

    Update at 2022-08-09T16:39:16.662052Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -8,7 +8,7 @@ package com.bytelegend;
  * Note that you should only fix the errors, don't modify the comments themselves.
  *
  * @author: Jonas3012
- * {@docRoot} java-fix-broken-javadoc\src\main\java\com\bytelegend\Challenge.java
+ * location C:\ users\ Documents 
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
  */
 public class Challenge {
