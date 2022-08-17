e4cf7d6180a74f07ef31f43c37b4023a3afb683c
Mongertaja <leilvu@gmail.com>
2022-08-17 13:10:09 +0000

    Update at 2022-08-17T13:10:09.961499Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,7 +9,7 @@ package com.bytelegend;
  * <p>
  * Location: C:{@literal \}users{@literal \}Documents
  * <p>
- * Fix the maximum number in three numbers, e.g. max(1,2,3) == 3
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
  */
 public class Challenge {
     /**
