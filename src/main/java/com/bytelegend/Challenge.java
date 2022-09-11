3da0b9592d7a845922eca782674197ec0d7df7f7
jenfnn <fwyjenny@gmail.com>
2022-09-11 03:40:33 +0000

    Update at 2022-09-11T03:40:33.529364Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,21 +9,14 @@ package com.bytelegend;
  * Location: C:\users\Documents
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
  */
-/** 
- * Find the maximum integer in three numbers.
- *
- * @param a 
- *   the first number
- * @param b 
-     *   the second number
- * @param c 
-     *   the third number
-     * @return max
-     *
-     */
-public class Challenge {
-  
 
+public class Challenge {
+    /** 
+    * Find the maximum integer in three numbers.
+    * @param a the first number
+    * @param b the second number
+    * @param c the third number
+    */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
     }
