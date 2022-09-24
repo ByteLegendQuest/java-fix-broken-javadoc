ae642c93bdf0fc2874f8fbd9b6bc21eaf2435dee
Zeon3 <1366685013@qq.com>
2022-09-24 12:45:24 +0000

    Update at 2022-09-24T12:45:24.598880Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,6 +9,7 @@ package com.bytelegend;
  *
  * Location: C:\users\Documents
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
+ *
  */
 public class Challenge {
     /**
@@ -17,6 +18,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
+     *
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
