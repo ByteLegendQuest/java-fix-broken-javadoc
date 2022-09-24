e20dfbfd720dd53673f15a15e2e0116dc84b694a
Zeon3 <1366685013@qq.com>
2022-09-24 12:59:27 +0000

    Update at 2022-09-24T12:59:27.739556Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,5 @@
 package com.bytelegend;
-/**
+/*
  * 本类中的注释和Javadoc存在一些问题，会使得`mvn verify`失败。请修复之，确保`mvn verify`通过。
  * 注意：请只修复错误，不要修改注释内容。
  *
@@ -11,7 +11,7 @@ package com.bytelegend;
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
  */
 public class Challenge {
-    /**
+    /*
      * Find the maximum integer in three numbers.
      *
      * @param a the first number
