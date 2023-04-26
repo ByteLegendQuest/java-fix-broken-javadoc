488eb29f5e426df09009bea1f73cac950f73e6d4
star1986 <ellen.yg@u.circlecat.org>
2023-04-26 06:43:52 +0000

    Update at 2023-04-26T06:43:52.527281Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,7 +17,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
-     * @return Returns the maximum integer among a, b and c.
+     * @return Returns
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
