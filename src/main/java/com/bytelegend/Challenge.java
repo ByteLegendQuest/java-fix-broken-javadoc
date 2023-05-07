e1c37a6881c778e3ec9d7cda4aa8e5d4aaea0ab1
wlonestar <68649094+wlonestar@users.noreply.github.com>
2023-05-07 02:39:25 +0000

    Update at 2023-05-07T02:39:25.585302Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -8,15 +8,16 @@ package com.bytelegend;
  * Note that you should only fix the errors, don't modify the comments themselves.
  *
  * Location: C:\users\Documents
- * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
+ * Find the maximum number in three numbers, e.g. max(1,2,3) -> 3
  */
 public class Challenge {
     /**
-     * Find the maximum integer in three numbers.
+     * Returns the maximum integer in three numbers.
      *
      * @param a the first number
      * @param b the second number
      * @param c the third number
+     * @return the maximum integer
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
