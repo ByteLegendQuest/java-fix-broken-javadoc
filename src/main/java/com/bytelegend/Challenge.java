9c634d18941234ff1009174fe5627884061ea9c7
calixtus <cc.snethlage@gmail.com>
2023-05-20 18:54:03 +0000

    Update at 2023-05-20T18:54:03.711502Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -8,10 +8,11 @@ package com.bytelegend;
  * Note that you should only fix the errors, don't modify the comments themselves.
  *
  * Location: C:\\users\\Documents
- * Fix the maximum number in three numbers, e.g. max(1,2,3) -> 3
+ * Fix the maximum number in three numbers, e.g. max(1,2,3) -&gt; 3
  */
 public class Challenge {
     /**
+     * Find the maximum integer in three numbers.
      * @param a the first number
      * @param b the second number
      * @param c the third number
