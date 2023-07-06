cba63fde87a94a8cbae95611e4f300f1f5c9a7b7
cai895015380 <65594155+cai895015380@users.noreply.github.com>
2023-07-06 21:37:41 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -17,10 +17,10 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
-     * @return the biggest number
+     * @return  the biggest number
+     * 
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
     }
 }
-
