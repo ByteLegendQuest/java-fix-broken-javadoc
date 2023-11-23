d4ae3211a8e625add71dfc98b2c298efe02212fb
Eloi0424 <beichu0424@qq.com>
2023-11-23 20:20:00 +0800

    fix javadoc

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,7 +7,7 @@ package com.bytelegend;
  * Please fix it and make sure `mvn verify` pass.
  * Note that you should only fix the errors, don't modify the comments themselves.
  *
- * Location: C:\\users\\Documents
+ * Location: C:{@literal \}users{@literal \}Documents
  * Fix the maximum number in three numbers, e.g. max(1,2,3) -{@literal >} 3
  */
 public class Challenge {
@@ -17,7 +17,7 @@ public class Challenge {
      * @param a the first number
      * @param b the second number
      * @param c the third number
-     * @return the maximum number
+     * @return max
      */
     public static int max(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
